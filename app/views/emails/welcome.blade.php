bfbcf148e0abdd9376104081f2099f3418136fd8
Julio Silvera <juliosilvera0@gmail.com>
2016-03-08 12:19:10 -0500

    cambios

--- app/views/emails/welcome.blade.php
+++ app/views/emails/welcome.blade.php
@@ -12,7 +12,7 @@
 $contenido = DB::table('campanias')->where('id', $id)->get();
 foreach ($contenido as $cont) {
 	echo $cont->texto;
-    echo "<img src='http://186.101.31.50/images/check_mailing?codigo=".$codigo."' style='visibility: hidden'>";
+    echo "<img src='http://186.101.31.50/mailing/public/images/check_mailing?codigo=".$codigo."' style='visibility: hidden'>";
 	echo "<center>Si no desea seguir recibiendo noticias nuestra hacer click <a href='http://mailing.gotrade.com.ec/delete/check_mailing?codigo=".$codigo."'>AQUI</a></center>";
 }
 ?>
